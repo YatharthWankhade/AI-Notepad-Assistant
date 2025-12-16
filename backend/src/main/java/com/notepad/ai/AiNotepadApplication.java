46255e47135072d6d8df4b7b0fac92a2fdb4c02a
YatharthWankhade <yatharthwankhade2001@gmail.com>
2025-12-17 01:05:58 +0530

    Backend Application

---
+++ backend/src/main/java/com/notepad/ai/AiNotepadApplication.java
@@ -0,0 +1,13 @@
+package com.notepad.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNotepadApplication.class, args);
+	}
+
+}
