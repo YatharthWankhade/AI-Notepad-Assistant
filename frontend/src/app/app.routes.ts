d2a2e70ad70e5d47936c429536d87ec85412a418
YatharthWankhade <yatharthwankhade2001@gmail.com>
2026-02-02 22:29:37 +0530

    App Frontend Changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { RegisterComponent } from './register/register.component';
+import { NotepadComponent } from './notepad/notepad.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LandingComponent },
+    { path: 'app', component: NotepadComponent },
+    { path: '**', redirectTo: '' }
+];
